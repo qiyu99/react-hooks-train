88a17d96ad20d4776886fa53b461e61902b6b6ea
quyang <jscoder@qq.com>
2020-05-18 15:19:13 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button type="primary">Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
